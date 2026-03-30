63d30ef61404b0de74609f3b008ea21fc588fc40
David Li <davidli21821@gmail.com>
2026-03-30 12:38:40 -0400

    update mac.v for parameterized widths

--- rtl/mac.v
+++ rtl/mac.v
@@ -6,20 +6,23 @@
  * multiplies two 8-bit inputs and adds to a 16-bit accumulator.
  */
 
-module mac (
+module mac #(
+    parameter IN_WIDTH = 8,
+    parameter OUT_WIDTH = 16
+)(
     input wire clk,              
     input wire rst_n,            // active low synch reset
     input wire en,        
     // 8 bit inputs       
-    input wire [7:0] a,         
-    input wire [7:0] b,          
+    input wire [IN_WIDTH-1:0] a,         
+    input wire [IN_WIDTH-1:0] b,          
     // 16 bit output
-    output reg [15:0] result     
+    output reg [OUT_WIDTH-1:0] result     
 );
 
     always @(posedge clk) begin
         if (!rst_n) begin
-            result <= 16'b0; // reset accumulator to zero when reset is pulled low
+            result <= {OUT_WIDTH{1'b0}}; // reset accumulator to zero when reset is pulled low
         end else if (en) begin
             result <= result + (a * b); // when enabled, multiply a and b and accumulate the result
         end
